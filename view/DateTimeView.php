eb0ec4586872ecd1e50e3353be53247a194eb9c9
Mathias Sundin <ms223uh@student.lnu.se>
2015-09-08 16:24:13 +0200

    Fix - Time test done!
    Fixs

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,14 @@ class DateTimeView {
 
 	public function show() {
 
+		date_default_timezone_set('Europe/Stockholm');
+
+		$dt = getdate();
+		$dt2 = date("H:m:s");
+ 		$showDate = "$dt[weekday], the $dt[mday]th of $dt[month] $dt[year], The time is $dt2";
+
 		$timeString = 'TODO, Write servertime here...';
 
-		return '<p>' . $timeString . '</p>';
+		return '<p>' . $showDate . '</p>';
 	}
 }
